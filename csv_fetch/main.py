607f9a87f330074ebab1195f4ebf92171462ca3c
Kibwe <54296006+kibwehoward@users.noreply.github.com>
2024-01-01 17:24:56 -0500

    Update main.py

--- csv_fetch/main.py
+++ csv_fetch/main.py
@@ -1,7 +1,7 @@
 import requests
 from tqdm import tqdm
 
-url = "path/to/static/CSV/file.csv"       # 'path/to/static/CSV/file.csv' is a temporary name. Change it to match the file's actual URL
+url = "path/to/static/file.csv"       # 'path/to/static/CSV/file.csv' is a temporary name. Change it to match the file's actual URL
 response = requests.get(url, stream=True)
 
 with open("file_name.csv", "wb") as file, tqdm(        # 'file_name.csv' is a temporary name. Change it to match the actual file name
