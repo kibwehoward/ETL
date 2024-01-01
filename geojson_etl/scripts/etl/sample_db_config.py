bb8ad486bd6755fca8d2298b5a1966f5346115e1
Kibwe <54296006+kibwehoward@users.noreply.github.com>
2024-01-01 16:51:37 -0500

    Create sample_db_config.py
    Added sample_db_config.py with template for database configuration settings

---
+++ geojson_etl/scripts/etl/sample_db_config.py
@@ -0,0 +1,7 @@
+db_credentials = {
+    'username': 'your_username',
+    'password': 'your_password',
+    'host': 'your_db_hostname_or_ip',
+    'port': 'your_db_port',
+    'database': 'your_db_name',
+}
