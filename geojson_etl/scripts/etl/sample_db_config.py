dfbd6b73e1d305b7d0940797a4f9a76ae1667092
Kibwe <54296006+kibwehoward@users.noreply.github.com>
2024-01-01 16:55:55 -0500

    Delete geojson_etl/scripts/etl/sample_db_config.py
    Deleted sample_db_config.py from geojson_etl/scripts and relocated it to the repository's root directory

--- geojson_etl/scripts/etl/sample_db_config.py
+++
@@ -1,7 +0,0 @@
-db_credentials = {
-    'username': 'your_username',
-    'password': 'your_password',
-    'host': 'your_db_hostname_or_ip',
-    'port': 'your_db_port',
-    'database': 'your_db_name',
-}
