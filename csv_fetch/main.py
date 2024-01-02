a53211b4ffc1ad999ed22d4411176acb7a6a7d87
Kibwe <54296006+kibwehoward@users.noreply.github.com>
2024-01-02 07:13:19 -0500

    Update main.py

--- csv_fetch/main.py
+++ csv_fetch/main.py
@@ -9,7 +9,7 @@ with open("file_name.csv", "wb") as file, tqdm(        # 'file_name.csv' is a te
         total=int(response.headers.get('content-length', 0)),
         unit='iB',
         unit_scale=True
-    ) as bar:
+) as bar:
     for data in response.iter_content(chunk_size=1024):
         size = file.write(data)
         bar.update(size)
