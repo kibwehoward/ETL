11d88d018e35108b8e9e38c4c67aaefa3e5762c5
Kibwe <54296006+kibwehoward@users.noreply.github.com>
2024-01-01 20:31:06 -0500

    Update main.py

--- geojson_etl/scripts/etl/main.py
+++ geojson_etl/scripts/etl/main.py
@@ -9,9 +9,9 @@ from db_config import db_credentials
 def main():
     """Main function to execute the script."""
     start_time = time.time()
-    geojson_url = "geojson/api/url.geojson"    # 'geojson/api/url.geojson' is a temporary name. Change it to match the API's actual URL.
-    page_size = 1000
-    delay = 1
+    geojson_url = "geojson/api/url.geojson"    # 'geojson/api/url.geojson' is a temporary name. Change it to match the API's actual URL
+    page_size = 1000                           # Be aware of any rate limits imposed by the API                            
+    delay = 1                                  # and design your data fetching strategy accordingly   
 
     total_records_fetched = fetch_and_insert_geodata(geojson_url, page_size, delay, db_credentials)
     logging.info(f"Total records fetched and inserted: {total_records_fetched}")
